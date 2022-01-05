de8cd706802ab83fd3b6366903e154e8bfecf91d
JisooSong <jeesoo9595@snu.ac.kr>
2022-01-05 10:29:55 +0900

    feat : implement DataclassWidget
    Use pytest.fixture for test

--- dataclass2PySide6/tests/test_dataclass_widget.py
+++ dataclass2PySide6/tests/test_dataclass_widget.py
@@ -1,9 +1,12 @@
 import dataclasses
+import pytest
 from PySide6.QtWidgets import QCheckBox, QLineEdit
 from dataclass2PySide6 import DataclassWidget, IntLineEdit, FloatLineEdit
 
 
-def test_DataclassWidget_construction(qtbot):
+@pytest.fixture
+def dclswidget(qtbot):
+
     @dataclasses.dataclass
     class DataClass:
         a: bool
@@ -17,39 +20,43 @@ def test_DataclassWidget_construction(qtbot):
         i: str = "foo"
 
     widget = DataclassWidget.fromDataclass(DataClass)
+    return widget
 
-    widget_a = widget.widgets()["a"]
+
+def test_DataclassWidget_construction(qtbot, dclswidget):
+
+    widget_a = dclswidget.widgets()["a"]
     assert widget_a.isCheckable()
     assert not widget_a.isChecked()
     assert widget_a.text() == "a"
     assert isinstance(widget_a, QCheckBox)
 
-    widget_b = widget.widgets()["b"]
+    widget_b = dclswidget.widgets()["b"]
     assert widget_b.placeholderText() == "b"
     assert widget_b.text() == ""
     assert isinstance(widget_b, IntLineEdit)
 
-    widget_c = widget.widgets()["c"]
+    widget_c = dclswidget.widgets()["c"]
     assert widget_c.placeholderText() == "c"
     assert widget_c.text() == ""
     assert isinstance(widget_c, FloatLineEdit)
 
-    widget_d = widget.widgets()["d"]
+    widget_d = dclswidget.widgets()["d"]
     assert widget_d.placeholderText() == "d"
     assert widget_d.text() == ""
     assert isinstance(widget_d, QLineEdit)
 
-    widget_e = widget.widgets()["e"]
+    widget_e = dclswidget.widgets()["e"]
     assert widget_e.isChecked()
 
-    widget_f = widget.widgets()["f"]
+    widget_f = dclswidget.widgets()["f"]
     assert not widget_f.isChecked()
 
-    widget_g = widget.widgets()["g"]
+    widget_g = dclswidget.widgets()["g"]
     assert widget_g.text() == "42"
 
-    widget_h = widget.widgets()["h"]
+    widget_h = dclswidget.widgets()["h"]
     assert widget_h.text() == "4.2"
 
-    widget_i = widget.widgets()["i"]
+    widget_i = dclswidget.widgets()["i"]
     assert widget_i.text() == "foo"
